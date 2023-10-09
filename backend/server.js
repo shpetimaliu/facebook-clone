39126c78ce10a6de8d84a2a341b46aa0b6769849
Shpetim <shpetimmaliu@gmail.com>
2023-10-09 20:23:47 +0200

    use dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
-const useRouters = require("./routers/user");
 const { readdirSync } = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
 
 // use
 app.use(cors());
 
 readdirSync("./routers").map((r) => app.use("/", require(`./routers/${r}`)));
 
-app.listen(3001, () => {
-  console.log("Server is running on 3001-Port");
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}-Port`);
 });
