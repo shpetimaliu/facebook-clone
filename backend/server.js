d0ecc293d23c944e0510f1d107331d467d3a72a4
Shpetim <shpetimmaliu@gmail.com>
2023-10-09 19:10:21 +0200

    Use cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const options = {
+  origin: "http://localhost:3000",
+  useSuccessStatus: 200,
+};
+
+app.use(cors(options));
 
 app.get("/", (req, res) => {
   res.send("Hello from server");
