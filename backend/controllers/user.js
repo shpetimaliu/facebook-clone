5621000562fc0628f848db6e19d0daa9fc0773af
Shpetim <shpetimmaliu@gmail.com>
2023-10-11 14:34:45 +0200

    add screenshot

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -92,14 +92,32 @@ exports.register = async (req, res) => {
 };
 
 exports.activateAccount = async (req, res) => {
-  const { token } = req.body;
-  const user = jwt.verify(token, process.env.TOKEN_SECRET);
-  console.log(user);
-  const check = await User.findById(user.id);
-  if (check.verified == true) {
-    return res.status(400).json({ message: "This email is already activated" });
-  } else {
-    await User.findByIdAndUpdate(user.id, { verified: true });
-    return res.status(200).json({ message: "Account has been activated" });
+  try {
+    const { token } = req.body;
+    const user = jwt.verify(token, process.env.TOKEN_SECRET);
+    console.log(user);
+    const check = await User.findById(user.id);
+    if (check.verified == true) {
+      return res
+        .status(400)
+        .json({ message: "This email is already activated" });
+    } else {
+      await User.findByIdAndUpdate(user.id, { verified: true });
+      return res.status(200).json({ message: "Account has been activated" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "This email not existing" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
--- backend/routers/user.js
+++ backend/routers/user.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const app = express();
 const router = express.Router();
 
-const { register, activateAccount } = require("../controllers/user");
+const { register, activateAccount, login } = require("../controllers/user");
 
 router.post("/register", register);
 router.post("/activate", activateAccount);
+router.post("/login", login);
 
 module.exports = router;
