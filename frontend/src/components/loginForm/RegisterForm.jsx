3294a2577cdc37edfbb11c0bc81a964fc7422dae
Shpetim <shpetimmaliu@gmail.com>
2023-10-12 03:06:27 +0200

    update

--- frontend/src/components/loginForm/RegisterForm.jsx
+++ frontend/src/components/loginForm/RegisterForm.jsx
@@ -2,24 +2,43 @@ import { Form, Formik } from "formik";
 import React, { useState } from "react";
 import RegisterInput from "../inputs/registerInput/registerInput";
 
-const userInfos = {
-  emri: "",
-  mbiemri: "",
-  email: "",
-  password: "",
-  ditlindjaViti: "",
-  ditlindjaMuaji: "",
-  ditlindjaDita: "",
-  gender: "",
-};
-
 function RegisterForm() {
-  const [user, setUser] = useState(userInfos);
+  const userInfos = {
+    emri: "",
+    mbiemri: "",
+    email: "",
+    password: "",
+    ditlindjaViti: new Date().getFullYear(),
+    ditlindjaMuaji: new Date().getMonth() + 1,
+    ditlindjaDita: new Date().getDate(),
+    gender: "",
+  };
 
+  const [user, setUser] = useState(userInfos);
+  const {
+    emri,
+    mbiemri,
+    email,
+    password,
+    ditlindjaViti,
+    ditlindjaMuaji,
+    ditlindjaDita,
+    gender,
+  } = user;
+  const vitiTemp = new Date().getFullYear();
   const handleRegisterChange = (e) => {
     const { name, value } = e.target;
     setUser({ ...user, [name]: value });
   };
+
+  const viti = Array.from(new Array(110), (val, index) => vitiTemp - index);
+  const muaji = Array.from(new Array(12), (val, index) => index + 1);
+  const NxjerrDiten = () => {
+    return new Date(ditlindjaViti, ditlindjaMuaji, 0).getDate();
+  };
+
+  const dita = Array.from(new Array(NxjerrDiten()), (val, index) => index + 1);
+  console.log(user);
   return (
     <div className="blur">
       <div className="register">
@@ -66,14 +85,38 @@ function RegisterForm() {
                   Date of birth <i className="info_icon"></i>
                 </div>
                 <div className="reg_grid">
-                  <select name="ditlindjaDita">
-                    <option>15</option>
+                  <select
+                    name="ditlindjaDita"
+                    value={ditlindjaDita}
+                    onChange={handleRegisterChange}
+                  >
+                    {dita.map((dit, i) => (
+                      <option value={dit} key={i}>
+                        {dit}
+                      </option>
+                    ))}
                   </select>
-                  <select name="ditlindjaMuaji">
-                    <option>15</option>
+                  <select
+                    name="ditlindjaMuaji"
+                    value={ditlindjaMuaji}
+                    onChange={handleRegisterChange}
+                  >
+                    {muaji.map((muaj, i) => (
+                      <option value={muaj} key={i}>
+                        {muaj}
+                      </option>
+                    ))}
                   </select>
-                  <select name="ditlindjaViti">
-                    <option>15</option>
+                  <select
+                    name="ditlindjaViti"
+                    value={ditlindjaViti}
+                    onChange={handleRegisterChange}
+                  >
+                    {viti.map((year, i) => (
+                      <option value={year} key={i}>
+                        {year}
+                      </option>
+                    ))}
                   </select>
                 </div>
               </div>
