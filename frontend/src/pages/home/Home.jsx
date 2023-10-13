301afebc501dd14aea0bc34db8311e59816c0a50
Shpetim <shpetimmaliu@gmail.com>
2023-10-13 18:09:11 +0200

    start creating function cliclOutside for hide

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import Header from "../../components/header/Header";
+import useClickOutside from "../../helpers/clickOutside";
 import "./Home.css";
 
 function Home() {
+  const el = useRef(null);
+  useClickOutside(el, () => {
+    el.current.style.display = "none";
+  });
+
   return (
     <div>
       <Header />
+      <div className="card" ref={el}></div>
     </div>
   );
 }
