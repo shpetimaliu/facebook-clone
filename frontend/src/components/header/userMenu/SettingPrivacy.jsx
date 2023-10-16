9f63e3e7557b9a6234e159252460d77b2fece334
Shpetim <shpetimmaliu@gmail.com>
2023-10-16 22:22:52 +0200

    up

--- frontend/src/components/header/userMenu/SettingPrivacy.jsx
+++ frontend/src/components/header/userMenu/SettingPrivacy.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function SettingPrivacy() {
-  return <div>settingPriivacy</div>;
+  return (
+    <div className="absolute_wrap">
+      <div className="absolute_wrap_header">
+        <div className="circle">
+          <i className="arrow_back_icon"></i>
+        </div>
+        Setting & Privacy
+      </div>
+      <div className="userMenu_item hover3">
+        <div className="small_circle">
+          <i className="settings_filled_icon"></i>
+        </div>
+        <span>Settings</span>
+      </div>
+    </div>
+  );
 }
 
 export default SettingPrivacy;
