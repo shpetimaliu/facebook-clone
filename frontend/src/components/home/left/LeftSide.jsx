57d077bc571954c816538acf847865734465fef9
Shpetim <shpetimmaliu@gmail.com>
2023-10-17 16:42:03 +0200

    up

--- frontend/src/components/home/left/LeftSide.jsx
+++ frontend/src/components/home/left/LeftSide.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { left } from "../../../data/home";
+import { ArrowDown1 } from "../../../svg";
 import LeftLink from "./LeftLink";
 import "./LeftSide.css";
 
 function LeftSide({ user }) {
+  const [visible, setVisible] = useState(false);
+
   return (
-    <div className="leftSide">
+    <div className="leftSide scrollbar">
       <Link to="/profile" className="leftLink hover1">
         <img src={user?.profili} alt="" />
         <span>
@@ -21,6 +24,52 @@ function LeftSide({ user }) {
           notify={link.notification}
         />
       ))}
+
+      {!visible && (
+        <div
+          className="leftLink hover1"
+          onClick={() => {
+            setVisible(true);
+          }}
+        >
+          <div className="small_circle">
+            <ArrowDown1 />
+          </div>
+          <span>See more</span>
+        </div>
+      )}
+      {visible && (
+        <div className="moreLeft">
+          {left.slice(8, left.length).map((link, i) => (
+            <LeftLink
+              key={i}
+              img={link.img}
+              text={link.text}
+              notify={link.notification}
+            />
+          ))}
+          <div
+            className="leftLink hover1"
+            onClick={() => {
+              setVisible(false);
+            }}
+          >
+            <div className="small_circle rotate360">
+              <ArrowDown1 />
+            </div>
+            <span>Show less</span>
+          </div>
+        </div>
+      )}
+      <div className="splitter">
+        <div className="shortcut">
+          <div className="heading">Your Shortcut</div>
+          <div className="edit">Edit</div>
+        </div>
+        <div className="shortcut_link">
+          <img src="" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/home/left/Shortcut.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Shortcut() {
+  return <div></div>;
+}
+
+export default Shortcut;
